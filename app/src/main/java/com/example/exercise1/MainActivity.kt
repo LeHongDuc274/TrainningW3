04f0be6b97fac2397c3b6aeab70d41bf2c161ffa
LeHongDuc274 <LeHongDuc274@gmail.com>
2021-08-18 20:36:33 +0700

    Shared mutable state and concurrency && change text color

--- app/src/main/java/com/example/exercise1/MainActivity.kt
+++ app/src/main/java/com/example/exercise1/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.exercise1
 
 import android.annotation.SuppressLint
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import android.util.Log
+
 import android.view.MotionEvent
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import androidx.core.view.MotionEventCompat
 import com.example.exercise1.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,6 +17,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private var oldY: Float = 0F
     private val viewModel: MainViewModel by viewModels()
+    private val listColor: MutableList<Int> = mutableListOf(
+        R.color.textColor1,
+        R.color.textColor2,
+        R.color.textColor3,
+        R.color.textColor4,
+        R.color.textColor5,
+        R.color.textColor6,
+        R.color.textColor7
+    )
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,16 +34,29 @@ class MainActivity : AppCompatActivity() {
 
         btn_increase?.setOnClickListener {
             viewModel.increase()
-            viewModel.state = false
-            viewModel.lastTimeClick = System.currentTimeMillis()
+            viewModel.state.set(false)
+            viewModel.lastTimeClick.set(System.currentTimeMillis())
         }
         btn_decrease?.setOnClickListener {
             viewModel.decrease()
-            viewModel.state = false
-            viewModel.lastTimeClick = System.currentTimeMillis()
+            viewModel.state.set(false)
+            viewModel.lastTimeClick.set(System.currentTimeMillis())
         }
         viewModel.num.observe(this, {
             tv.text = it.toString()
+        })
+        viewModel.changeColor.observe(this, {
+            if (it) {
+                var rand: Int = R.color.red
+                while (ContextCompat.getColor(this, rand) == tv.currentTextColor) {
+                    rand = listColor.random().toInt()
+                }
+                try {
+                    tv.setTextColor(ContextCompat.getColor(this, rand))
+                } catch (e : Resources.NotFoundException){
+                    tv.setTextColor(ContextCompat.getColor(this, R.color.textColor5))
+                }
+            }
         })
 
         linear_layout.setOnTouchListener { _, event ->
@@ -40,21 +64,20 @@ class MainActivity : AppCompatActivity() {
             when (action) {
                 MotionEvent.ACTION_DOWN -> {
                     oldY = event.y
-                    viewModel.state = false
-                    viewModel.lastTimeClick = null
-                    Log.d("Tagd", "Action was DOWN ${viewModel.lastTimeClick}")
+//                    if ((viewModel.num.value?.minus(viewModel.lastNumberChangeColer))!! > 100) {
+//                        viewModel.lastNumberChangeColer = viewModel.num.value!!
+//                        viewModel._changeColor.value = true
+//                    }
+                    viewModel.isChangeColer()
+                    viewModel.state.set(false)
+                    viewModel.lastTimeClick.set(0L)
                     true
                 }
                 MotionEvent.ACTION_MOVE -> {
-                    Log.d(
-                        "Tagd",
-                        "Action was move ${viewModel.lastTimeClick} ${viewModel.state}"
-                    )
                     val curY = event.y
                     val deltaY = curY.minus(oldY)
                     oldY = curY
-
-                    Log.d("Tagd", "Action was MOVE cur-$curY old $oldY deta $deltaY")
+                    viewModel._changeColor.value = false
 
                     if (deltaY > 5) {
                         viewModel.decrease()
@@ -65,16 +88,11 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
                 MotionEvent.ACTION_UP -> {
-                    viewModel.lastTimeClick = System.currentTimeMillis()
-                    Log.d(
-                        "Tagd",
-                        "Action was up ${viewModel.lastTimeClick}  ${viewModel.state}"
-                    )
+                    viewModel.lastTimeClick.set(System.currentTimeMillis())
                     true
                 }
-                MotionEvent.ACTION_OUTSIDE -> false
-                else -> false
-                //                    else -> super.onTouchEvent(event)
+                MotionEvent.ACTION_OUTSIDE -> true
+                else -> true
             }
         }
 
--- app/src/main/java/com/example/exercise1/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/exercise1/viewmodel/MainViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.exercise1.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicLong
+import kotlin.math.log
 
 class MainViewModel : ViewModel() {
     private val job1 = Job()
@@ -12,33 +16,48 @@ class MainViewModel : ViewModel() {
     private val _num: MutableLiveData<Int> = MutableLiveData(0)
     val num: LiveData<Int> = _num
 
-    var state: Boolean = false
-    var lastTimeClick: Long? = null
+    var state= AtomicBoolean(false) //Shared mutable state and concurrency
+
+    var lastTimeClick = AtomicLong(0L)
+
+    var lastNumberChangeColer : Int =0
+
+    val _changeColor : MutableLiveData<Boolean> = MutableLiveData(false)
+    val changeColor : LiveData<Boolean> = _changeColor
 
     init {
         viewModelScope.launch(Dispatchers.Default + job1) {
             while (isActive) {
-                delay(100)
-                if (lastTimeClick != null && ((System.currentTimeMillis() - lastTimeClick!!) >= 900))
-                    state = true
+                delay(50L)
+                if (lastTimeClick.get() != 0L  &&
+                    ((System.currentTimeMillis() - lastTimeClick.get()) >= 900L))
+                    state.set(true)
             }
         }
 
         viewModelScope.launch(Dispatchers.Default + job2) {
             while (isActive) {
                 delay(50L)
-                if (state) {
+                if (state.get()) {
                     if (_num.value!! > 0) {
                         decrease()
                     } else if (_num.value!! < 0) {
                         increase()
                     }
-                    // delay(100)
+                    isChangeColer()
                 }
             }
         }
     }
-
+    fun isChangeColer(){
+        if (Math.abs((num.value?.minus(lastNumberChangeColer))!!) > 100) {
+            Log.e("tag","${lastNumberChangeColer}")
+            lastNumberChangeColer = num.value!!
+            _changeColor.postValue(true)
+        } else{
+            _changeColor.postValue(false)
+        }
+    }
     fun increase() {
         _num.postValue(_num.value?.plus(1))
     }
@@ -46,6 +65,5 @@ class MainViewModel : ViewModel() {
     fun decrease() {
         _num.postValue(_num.value?.minus(1))
     }
-
 
 }
