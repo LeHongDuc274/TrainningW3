39b2b111c705201724a9e33994f7bd224c9dba3e
LeHongDuc274 <LeHongDuc274@gmail.com>
2021-08-20 13:17:46 +0700

    commit branch 3

--- app/src/main/java/com/example/exercise1/MainActivity.kt
+++ app/src/main/java/com/example/exercise1/MainActivity.kt
@@ -4,69 +4,98 @@ import android.annotation.SuppressLint
 import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.view.MotionEvent
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.core.view.MotionEventCompat
 import com.example.exercise1.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 
 
 class MainActivity : AppCompatActivity() {
     private var oldY: Float = 0F
     private val viewModel: MainViewModel by viewModels()
 
+
+
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_increase?.setOnClickListener {
-            viewModel.increase()
-            onButtonClick()
+        btn_increase?.setOnTouchListener { v, event ->
+            val action: Int = MotionEventCompat.getActionMasked(event)
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    state.set(false)
+                    stopThreadCoundownt()
+                    increase()
+                   isChangeColer()
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    state.set(true)
+                    startThreadCoundownt()
+                    true
+                }
+                else -> true
+            }
         }
-        btn_decrease?.setOnClickListener {
-            viewModel.decrease()
-            onButtonClick()
+        btn_decrease?.setOnTouchListener { v, event ->
+            val action: Int = MotionEventCompat.getActionMasked(event)
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    stopThreadCoundownt()
+                    decrease()
+                    isChangeColer()
+                   state.set(false)
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    state.set(true)
+                    startThreadCoundownt()
+                    true
+                }
+                else -> true
+            }
         }
-
-        observeData()
 
         linear_layout.setOnTouchListener { _, event ->
             val action: Int = MotionEventCompat.getActionMasked(event)
             when (action) {
                 MotionEvent.ACTION_DOWN -> {
                     oldY = event.y
-                    viewModel.isChangeColer()
-                    viewModel.state.set(false)
-                    viewModel.lastTimeClick.set(0L)
+                    isChangeColer()
+                    state.set(false)
+                    stopThreadCoundownt()
                     true
                 }
                 MotionEvent.ACTION_MOVE -> {
                     val curY = event.y
                     val deltaY = curY.minus(oldY)
                     oldY = curY
-                    viewModel._changeColor.value = false
                     if (deltaY > 5) {
-                        viewModel.decrease()
+                       decrease()
                     } else if (deltaY < -5) {
-                        viewModel.increase()
+                       increase()
                     }
                     true
                 }
                 MotionEvent.ACTION_UP -> {
-                    viewModel.lastTimeClick.set(System.currentTimeMillis())
+                    state.set(true)
+                    startThreadCoundownt()
                     true
                 }
                 else -> true
             }
         }
     }
-
-    private fun observeData() {
-        viewModel.num.observe(this, {
-            tv.text = it.toString()
-        })
 
         viewModel.changeColor.observe(this, {
             if (it) {
@@ -76,17 +105,76 @@ class MainActivity : AppCompatActivity() {
                 }
                 try {
                     tv.setTextColor(ContextCompat.getColor(this, rand))
-                } catch (e: Resources.NotFoundException) {
+                } catch (e : Resources.NotFoundException){
                     tv.setTextColor(ContextCompat.getColor(this, R.color.textColor5))
                 }
             }
         })
     }
 
-    private fun onButtonClick() {
-        viewModel.state.set(false)
-        viewModel.lastTimeClick.set(System.currentTimeMillis())
-        viewModel.isChangeColer()
+    var num : Int =0
+
+    var state = AtomicBoolean(false) //Shared mutable state and concurrency
+
+    var lastNumberChangeColer = AtomicInteger(0)
+
+    val changeColor: AtomicBoolean = AtomicBoolean(false)
+
+    var threadCd = Thread()
+    val handler =object : Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            when(msg.what){
+                MSG_UPDATE_TV ->{
+                    tv.text = msg.arg1.toString()
+                }
+                else -> Unit
+            }
+        }
+
+    }
+
+    val runnable = Runnable {
+        while (state.get()) {
+            try {
+                Thread.sleep(50L)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+            if (num < 0) increase()
+            else if (num > 0) decrease()
+            isChangeColer()
+        }
+    }
+
+
+    fun stopThreadCoundownt() {
+        handler.removeCallbacksAndMessages(null)
+        if (threadCd.isInterrupted == false) threadCd.interrupt()
+    }
+
+    fun startThreadCoundownt() {
+        threadCd = Thread(runnable)
+        handler.postDelayed({ threadCd.start() }, 1000L)
+    }
+
+
+    fun isChangeColer() {
+        if (Math.abs((num.minus(lastNumberChangeColer.get()))) > 100) {
+//            Log.e("tag", "${lastNumberChangeColer}")
+            lastNumberChangeColer = num
+            changeColor.set(true)
+        } else {
+            changeColor.set(false)
+        }
+    }
+
+    fun increase() {
+        num = (num.plus(1))
+    }
+
+    fun decrease() {
+        num = (num.minus(1))
     }
 
     private val listColor: MutableList<Int> = mutableListOf(
@@ -98,10 +186,9 @@ class MainActivity : AppCompatActivity() {
         R.color.textColor6,
         R.color.textColor7
     )
-
-//    override fun onTouchEvent(event: MotionEvent?): Boolean {
-//
-//    }
-
+companion object{
+    const val MSG_UPDATE_TV = 1
+    const val MSG_CHANGCOLOR = 2
+}
 
 }
--- app/src/main/java/com/example/exercise1/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/exercise1/viewmodel/MainViewModel.kt
@@ -1,68 +1,13 @@
 package com.example.exercise1.viewmodel
 
-import android.util.Log
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.*
 import java.util.concurrent.atomic.AtomicBoolean
-import java.util.concurrent.atomic.AtomicLong
 
 class MainViewModel : ViewModel() {
-    private val job1 = Job()
-    private val job2 = Job()
-    private val _num: MutableLiveData<Int> = MutableLiveData(0)
-    val num: LiveData<Int> = _num
-
-    // countdown state
-    var state= AtomicBoolean(false) //Shared mutable state and concurrency
-    var lastTimeClick = AtomicLong(0L)
-
-    var lastNumberChangeColer : Int =0
-
-    val _changeColor : MutableLiveData<Boolean> = MutableLiveData(false)
-    val changeColor : LiveData<Boolean> = _changeColor
-
-    init {
-        viewModelScope.launch(Dispatchers.Default + job1) {
-            while (isActive) {
-                delay(50L)
-                if (lastTimeClick.get() != 0L  &&
-                    ((System.currentTimeMillis() - lastTimeClick.get()) >= 900L))
-                    state.set(true)
-            }
-        }
-
-        viewModelScope.launch(Dispatchers.Default + job2) {
-            while (isActive) {
-                delay(50L)
-                if (state.get()) {
-                    if (_num.value!! > 0) {
-                        decrease()
-                    } else if (_num.value!! < 0) {
-                        increase()
-                    }
-                    isChangeColer()
-                }
-            }
-        }
-    }
-    fun isChangeColer(){
-        if (Math.abs((num.value?.minus(lastNumberChangeColer))!!) > 100) {
-            Log.e("tag","${lastNumberChangeColer}")
-            lastNumberChangeColer = num.value!!
-            _changeColor.postValue(true)
-        } else{
-            _changeColor.postValue(false)
-        }
-    }
-    fun increase() {
-        _num.postValue(_num.value?.plus(1))
-    }
-
-    fun decrease() {
-        _num.postValue(_num.value?.minus(1))
-    }
 
 }
